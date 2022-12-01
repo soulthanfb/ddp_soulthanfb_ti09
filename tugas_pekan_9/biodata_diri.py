edb0dd1064ff03c50331aee9ba465d8ffbb4c431
Soulthan Fadhlillah Bagus <114553154+soulthanti04@users.noreply.github.com>
2022-12-02 06:43:03 +0700

    tugas_pekan_9

---
+++ tugas_pekan_9/biodata_diri.py
@@ -0,0 +1,12 @@
+def biodata(nama ="Soulthan Fadhlillah Bagus", alamat ="Cibinong, Bogor", tanggal = "04 Agustus 2004", umur = "18", tinggi = "173"):
+    beratIdeal = str((int(tinggi)-100)-((int(tinggi)-100)* 0.10))
+
+    
+    print("Nama               : " +nama)
+    print("Alamat             : " +alamat)
+    print("Tanggal            : " +tanggal)
+    print("Umur               : " +umur)
+    print("Tinggi Badan       : " +tinggi)
+    print("Berat Badan Ideal  : " +beratIdeal)
+
+biodata()
