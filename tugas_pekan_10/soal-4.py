7a3962629709e01c638a5ccc5de7a2655e5d8c24
Soulthan Fadhlillah Bagus <114553154+soulthanfb@users.noreply.github.com>
2022-12-09 15:24:46 +0700

    Add files via upload
    tugas-pekan-10

---
+++ tugas_pekan_10/soal-4.py
@@ -0,0 +1,10 @@
+#konsonan dari string
+def delVowel(inp):
+    vowel = ['a','i','u','e','o', ' ']
+    outp = ""
+
+    for character in inp:
+        if character.lower() not in vowel:
+            outp += character
+    return outp
+print(delVowel("Nurul Fikri"))
